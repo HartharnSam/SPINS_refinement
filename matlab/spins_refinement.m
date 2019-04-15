22e8fe274c5748adbed567c94e4af07a71653d40
Andrew Grace <andrew.grace@uwaterloo.ca>
2019-04-15 11:46:05 -0400

    Updated spins_refinement.m to fix permute grid error

--- matlab/spins_refinement.m
+++ matlab/spins_refinement.m
@@ -2,10 +2,9 @@
 % bottom boundary.
 
 clearvars
-nx = 2;             % increase x resolution nx times
-nz = 2;             % increase z resolution nz times
-ii = 10;           % number of output to interpolate
-method = 'cubic';       % Matlab built-in interpolation method
+n = 4;              % n times of original resolution
+ii = 2;           % number of output to interpolate
+method = 'spline';       % Matlab built-in interpolation method
                         % 'nearest', 'linear', 'spline' or 'cubic'
 
 %% Read in old spins.conf, grids and fields
@@ -16,6 +15,13 @@ den = spins_reader('rho',ii);
 u = spins_reader('u',ii);
 w = spins_reader('w',ii);
 
+%transpose so the ordering is correct
+x = x';
+z = z';
+u = u';
+w = w';
+den = den';
+
 Lx = params.Lx;
 Lz = params.Lz;
 Nx = params.Nx;
@@ -25,11 +31,8 @@ type_z = params.type_z;
 dx = x(2,2) - x(1,1);
 
 %% Define new grids
-Nx_new = nx*Nx;      % nx times resolution in x
-Nz_new = nz*Nz;      % nz times resolution in z
-
-params.('Nx') = Nx_new;    % Update resolution in spins parameters struct
-params.('Nz') = Nz_new;    % Update resolution in spins parameters struct
+Nx_new = n*Nx;      % n times resolution in x
+Nz_new = n*Nz;      % n times resolution in z
 
 x1d = Lx*(0.5:Nx_new-0.5)/Nx_new;           % periodic/free-slip in x
 switch type_z
@@ -55,9 +58,9 @@ z1 = [2*z1(1,:)-z1(3,:);        2*z1(1,:)-z1(2,:);      z1; ...
 switch type_x
     case 'FREE_SLIP'
         % Dirichlet for u, Neumann for v, w and den in x
-        u1   = [ -u(:,2)   -u(:,1)   u    -u(:,end-1)   -u(:,end)];
-        w1   = [  w(:,2)    w(:,1)   w     w(:,end-1)    w(:,end)];
-        den1 = [den(:,2)  den(:,1)  den  den(:,end-1)  den(:,end)];
+        u1   = [ -u(:,2)   -u(:,1)   u    -u(:,end)   -u(:,end-1)];
+        w1   = [  w(:,2)    w(:,1)   w     w(:,end)    w(:,end-1)];
+        den1 = [den(:,2)  den(:,1)  den  den(:,end)  den(:,end-1)];
     case 'FOURIER'
         % Periodic in x-direction
         u1   = [  u(:,(end-1):end)   u     u(:,1:2)];
@@ -69,7 +72,7 @@ end
 u1   = [  u1(2,:);   u1(1,:);  u1;    u1(end,:);   u1(end-1,:)];
 w1   = [ -w1(2,:);  -w1(1,:);  w1;   -w1(end,:);  -w1(end-1,:)];
 den1 = [den1(2,:); den1(1,:); den1; den1(end,:); den1(end-1,:)];
-
+%%
 % Interpolation
 u2   = interp2(x1,z1,u1,x2d,z2d,method);
 w2   = interp2(x1,z1,w1,x2d,z2d,method);
@@ -90,14 +93,34 @@ fid = fopen('w.orig','wb'); fwrite(fid,wspins,'double'); fclose(fid);
 
 %% Write parameters to spins.conf
 
-
 fid = fopen('spins.conf','wt');
-for i=1:numel(fieldnames(params))
-    fields=fieldnames(params);
-    fieldname = char(fields(i));
-    value = string(params.(fieldname));
-    fprintf(fid,'%s=%f \n', fieldname, value)
+fprintf(fid,'Lx = %12.8f \n',Lx);
+fprintf(fid,'Lz = %12.8f \n',Lz);
+fprintf(fid,'Nx = %d \n',Nx);
+fprintf(fid,'Nz = %d \n',Nz);
+fprintf(fid,'type_x = %s \n',type_x);
+fprintf(fid,'type_z = %s \n',type_z);
 
-end
+fprintf(fid,'min_x = %12.8f\n',0);
+fprintf(fid,'min_z = %12.8f\n',0);
+fprintf(fid,'mapped_grid = false\n');
 
-% quit
+fprintf(fid,'file_type = MATLAB\n');
+fprintf(fid,'u_file = u.orig\n');
+fprintf(fid,'w_file = w.orig\n');
+fprintf(fid,'rho_file = rho.orig\n');
+
+fprintf(fid,'g = %12.8f \n',params.g);
+fprintf(fid,'rot_f = %12.8f \n',params.rot_f);
+fprintf(fid,'rho_0 = %12.8f \n',1000);
+fprintf(fid,'visco = %12.8f \n',params.visco);
+fprintf(fid,'kappa_rho = %12.8f \n',params.kappa_rho);
+fprintf(fid,'perturb = %f \n',params.perturb);
+
+% fprintf(fid,'init_time = %12.8f\n',init_time);
+fprintf(fid,'final_time = %12.8f\n',params.final_time);
+fprintf(fid,'plot_interval = %12.8f \n',params.plot_interval);
+fprintf(fid,'restart = false \n');
+fclose(fid);
+
+%quit
